4cf15a5b13f41a9c6ef0d3c0e6e766bc6df62727
Lucy Wilcox <l.y.wilcox@gmail.com>
2018-02-22 21:03:30 -0500

    added read

--- snl_shell/snl.c
+++ snl_shell/snl.c
@@ -7,12 +7,18 @@ Created for Software Systems project 1
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
-int MAXARGS = 100;
+
+// todo: this is it folks, only 100 commands at a time
+#define MAXARGS 100
 
 
-// splits line into set of args
-void lsh_split_line(char* line, char** args){
+/* splits line into set of args
+*/
+void snl_split_line(char* line, char** args){
 	for (int i = 0; i < MAXARGS) {
 		// get each arg seperated by space
 		args[i] = strsep(&line, " "); 
@@ -25,9 +31,20 @@ void lsh_split_line(char* line, char** args){
 	}
 }
 
+/* based off of Brennan
+returns char* because allocation is done by getline
+*/
+char* snl_read_line(void){
+	char* line; // todo: maybe needs to equal null??
+	ssize_t buffersize = 0;
+	getline(&line, &buffersize, stdin);
+	return line;
+}
 
-// Using slight adaption of Brennan's implementation of the
-//  main loop https://brennan.io/2015/01/16/write-a-shell-in-c/
+/* Using slight adaption of Brennan's implementation of the
+  main loop https://brennan.io/2015/01/16/write-a-shell-in-c/
+
+*/
 void snl_loop(void){
   char *line;
   char **args[MAXARGS];
@@ -44,8 +61,9 @@ void snl_loop(void){
   } while (status);
 }
 
-// Using Brennan's implementation of main
-// https://brennan.io/2015/01/16/write-a-shell-in-c/
+/* Using Brennan's implementation of main
+ https://brennan.io/2015/01/16/write-a-shell-in-c/
+*/
 int main(int argc, char **argv){
   // Load config files, if any.
 
